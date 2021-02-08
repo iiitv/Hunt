0cd946743519c50c41f8af58afe63edff51467b1
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2021-02-08 08:45:00 +0530

    Running migrations

--- backend/settings.py
+++ backend/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
