a267dcdba7d2efaba023296bd1c3bb837a111044
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2021-02-15 11:20:13 +0530

    Changed time

--- api/scheduler.py
+++ api/scheduler.py
@@ -9,7 +9,8 @@ def start_job():
     print("Number of jobs = ", len(levels))
     i = 0
     for level in levels:
-        now = datetime.datetime.now() + datetime.timedelta(seconds = 3601)
-        scheduler.add_job(level.score_decay, 'interval', start_date = str(now)[:-7], minutes=60)
+        now = datetime.datetime(2021, 2, 19, 18, 0, 0, 0) + datetime.timedelta(seconds = 3601)
+        print(str(now))
+        scheduler.add_job(level.score_decay, 'interval', start_date = str(now), minutes=60)
         i+=1
     scheduler.start()
